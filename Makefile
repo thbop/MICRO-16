5bd445560f6048f33449c926a87d84661f7dc0c1
Thbop <75861321+thbop@users.noreply.github.com>
2025-06-16 17:02:44 -0400

    Added license + some untested read/write stuff

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 CC = g++
 CFLAGS = -g -Wall -fdiagnostics-color=always -Isrc -Iinclude
 LDFLAGS = -Llib -lSDL3
+SOURCES = Micro16.cpp $(wildcard src/**/*.cpp)
 
 all: micro16
 
 micro16:
 	mkdir -p build
-	$(CC) $(CFLAGS) Micro16.cpp $(LDFLAGS) -o build/Micro16.exe
+	$(CC) $(CFLAGS) $(SOURCES) $(LDFLAGS) -o build/Micro16.exe
--- src/btp6000/Btp.hpp
+++ src/btp6000/Btp.hpp
@@ -1,7 +1,31 @@
+/*
+* Copyright © 2025 Micro-16 Team
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the “Software”), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do
+* so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 #ifndef BTP_HPP
 #define BTP_HPP
 
 #include "stdint.h"
+
+#include "../bob3000/Bob.hpp"
 
 // The Better Than Pico 6000 namespace
 namespace btp {
@@ -20,12 +44,14 @@ namespace btp {
         //     Accumulator
         //     Base register
         //     X-index register
+        //     Memory register
         Register A, B, X;
 
-        // Stack registers
+        // Pointer registers
+        //     Instruction pointer
         //     Stack pointer
         //     Base pointer
-        uint16_t SP, BP;
+        uint16_t IP, SP, BP;
 
         // Segment registers
         //     Stack segment
@@ -33,11 +59,74 @@ namespace btp {
         //     Data segment
         uint16_t SS, CS, DS;
 
-        // Resets the CPU
+        // Default constructor
         BetterThanPico() {
+            Reset();
+        }
+
+        // Sets up the memory
+        void SetMemory( Bob3k *memory ) {
+            this->memory = memory;
+        }
+
+        void Reset() {
             // Set everything to zero
             A.value = B.value = X.value = 
-            SP = BP = SS = CS = DS = 0;
+            IP = SP = BP = SS = CS = DS = 0;
+        }
+
+        // Executes the fetched instruction
+        void Execute();
+
+    private:
+        Bob3k *memory;
+
+        // Calculates an address from a segment and an offset
+        // Similar to x86 memory segmentation:
+        //     0xFFF segments
+        //     Each segment is 0xFFF bytes wide
+        //     Segments are separated by 0x10 bytes
+        //
+        // For example:
+        //     FFF:00F = FFFF
+        //     FF0:0FF = FFFF
+        //     FFF:FFF = 0FEF
+        uint16_t CalculateAddress( uint16_t segment, uint16_t offset ) {
+            return ( segment << 4 ) + ( offset & 0xFFF );
+        }
+
+        // Read a byte given a segment and an offset
+        uint8_t Read( uint16_t segment, uint16_t offset ) {
+            return memory->Read( CalculateAddress( segment, offset ) );
+        }
+
+        // Read a word given a segment and an offset
+        uint16_t Read16( uint16_t segment, uint16_t offset ) {
+            return memory->Read16( CalculateAddress( segment, offset ) );
+        }
+
+        // Write a byte given a segment and an offset
+        void Write( uint16_t segment, uint16_t offset, uint8_t value ) {
+            memory->Write( CalculateAddress( segment, offset ), value );
+        }
+
+        // Write a word given a segment and an offset
+        void Write16( uint16_t segment, uint16_t offset, uint16_t value ) {
+            memory->Write16( CalculateAddress( segment, offset ), value );
+        }
+
+        // Fetches a byte from CS:IP and increments IP
+        uint8_t Fetch() {
+            uint8_t value = Read( CS, IP );
+            IP++;
+            return value;
+        }
+
+        // Fetches a word from CS:IP and increments IP
+        uint16_t Fetch16() {
+            uint16_t value = Read16( CS, IP );
+            IP += 2;
+            return value;
         }
     };
 
